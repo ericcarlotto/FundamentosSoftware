39618a09f3b1383575c2294c2c4b9b9a36029cfa
DESKTOP-5LSHH6H\Pichau <eric_carlotto@estudante.sc.senai.br>
2020-08-24 16:07:50 -0300

    Classe App com método main e interface com o usu.

---
+++ App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a placa do veículo desejado: ");
+        String placaSel = sc.nextLine();
+        if (placaSel==placa) {
+            System.out.print(veiculo);
+        }else`{
+            System.out.println("Veiculo não encontrado. Tente outra placa.");
+        }
+    }
+}
